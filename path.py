3427279adb907b988cd97bffdc81fcb892d35b5c
ABarashkov <alexander.stcstudent@gmail.com>
2023-07-02 12:31:07 +0400

    test_pdf code refactor, path.py implementation,  home assignment fix

---
+++ path.py
@@ -0,0 +1,5 @@
+from os import path
+
+dir_res = path.abspath(
+    path.join(path.dirname(__file__), 'resources')
+)
--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,10 +1,12 @@
 import os.path
 
 from pypdf import PdfReader
+from path import dir_res
 
 
 def test_pdf():
-    reader = PdfReader(os.path.abspath('../resources/docs-pytest-org-en-latest.pdf'))
+    pdf_file = os.path.join(dir_res, 'docs-pytest-org-en-latest.pdf')
+    reader = PdfReader(pdf_file)
     number_of_pages = len(reader.pages)
     page = reader.pages[0]
     text = page.extract_text()
