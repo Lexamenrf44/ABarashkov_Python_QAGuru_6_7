5491d3391e911237c63a8acd6b7e169cfb60293f
ABarashkov <alexander.stcstudent@gmail.com>
2023-07-02 12:35:17 +0400

    test_xlsx code refactor, path.py implementation,  home assignment fix

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,10 +1,12 @@
 import os.path
 from openpyxl import load_workbook
+from path import dir_res
 
 
 def test_read_xlsx_file():
     # Задаем уникальный путь
-    xlsx_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources/file_example_XLSX_50.xlsx'))
+    xlsx_file = os.path.join(dir_res, 'file_example_XLSX_50.xlsx')
+    # xlsx_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resources/file_example_XLSX_50.xlsx'))
     workbook = load_workbook(xlsx_file)
     sheet = workbook.active
     headers = [cell.value for cell in sheet[1]]
